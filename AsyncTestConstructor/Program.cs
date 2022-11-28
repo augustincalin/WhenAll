be6f74578745322c598a581aca143d8889f30dd5
Augustin Calin <augustin.calin@gmail.com>
2022-11-28 12:37:43 +0100

    adds exception.

--- AsyncTestConstructor/Program.cs
+++ AsyncTestConstructor/Program.cs
@@ -6,26 +6,47 @@ namespace AsyncTestConstructor
     {
         public static async Task Main(string[] args)
         {
+            ClassA myClass;
             var swatch = new Stopwatch();
             swatch.Start();
             var myTasker = new Tasker();
 
-            //! VER 1
-            var result1 = await myTasker.GetStringAsync(5);
-            var result2 = await myTasker.GetStringAsync(10);
-            var result3 = await myTasker.GetStringAsync(15);
-            var myA = new ClassA(result1, result2, result3);
+            //! VER 1 - parallel
+            var result1Task = myTasker.GetStringAsync(5);
+            var result2Task = myTasker.GetStringAsync(10);
+            var result3Task = myTasker.GetStringAsync(15);
 
-            //! VER 2
-            ////var task1 = myTasker.GetStringAsync(5);
-            ////var task2 = myTasker.GetStringAsync(10);
-            ////var task3 = myTasker.GetStringAsync(15);
+            var result1 = await result1Task;
+            var result2 = await result2Task;
+            var result3 = await result3Task;
 
-            ////var result = await Task.WhenAll(task1, task2, task3);
-            ////var myA = new ClassA(result[0], result[1], result[2]);
+            myClass = new ClassA(result1, result2, result3);
+            //this is the same as above...
+            //myClass = new ClassA(await result1Task, await result2Task, await result3Task);
+
+            //! VER 2 - sequential
+
+            //var result1 = await myTasker.GetStringAsync(5);
+            //var result2 = await myTasker.GetStringAsync(10);
+            //var result3 = await myTasker.GetStringAsync(15);
+
+            //myClass = new ClassA(result1, result2, result3);
+
+            //! VER 3 - parallel with exception
+            //? some question
+            //x some removed
+            // some removed
+            //var result1Task = myTasker.GetStringAsync(5);
+            //var result2Task = myTasker.GetStringAsync(10);
+            //var result3Task = myTasker.GetStringAsync(15);
+            //ar result = await Task.WhenAll(result1Task, result2Task, result3Task);
+
+            //myClass = new ClassA(result[0], result[1], result[2]);
+
+            Console.WriteLine(myClass);
 
             swatch.Stop();
-            Console.WriteLine(swatch.Elapsed);
+            Console.WriteLine($"Ellapsed time: {swatch.Elapsed}");
         }
     }
 }
--- AsyncTestConstructor/Tasker.cs
+++ AsyncTestConstructor/Tasker.cs
@@ -11,11 +11,16 @@ namespace AsyncTestConstructor
         public async Task<string> GetStringAsync(int delaySeconds)
         {
             return await Task.Run(async () => {
-                Console.WriteLine("Start processing GetStringAsync");
+                Console.WriteLine($"Start processing GetStringAsync with delay {delaySeconds}");
+                ////if (delaySeconds == 10)
+                ////{
+                ////    throw new Exception();
+                ////}
+
                 await Task.Delay(delaySeconds);
                 Thread.Sleep(delaySeconds * 1000);
-                Console.WriteLine("End processing GetStringAsync");
-                return "task";
+                Console.WriteLine($"End processing GetStringAsync with delay {delaySeconds}");
+                return $"task_{delaySeconds}";
             });
         }
     }
